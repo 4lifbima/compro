7765857b45d6418214c5c79dde6a8537c6a2a7e1
4lifbima <alif67916@gmail.com>
2024-11-28 08:46:35 +0800

    add database

---
+++ application/logs/log-2024-10-04.php
@@ -0,0 +1,33 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2024-10-04 21:34:01 --> 404 Page Not Found: Public/uploads
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2024-10-04 21:34:01 --> 404 Page Not Found: Public/uploads
+ERROR - 2024-10-04 21:34:01 --> 404 Page Not Found: Public/uploads
+ERROR - 2024-10-04 21:34:01 --> 404 Page Not Found: Public/uploads
+ERROR - 2024-10-04 21:34:01 --> 404 Page Not Found: Public/uploads
+ERROR - 2024-10-04 21:35:39 --> 404 Page Not Found: Public/uploads
+ERROR - 2024-10-04 21:35:39 --> 404 Page Not Found: Public/uploads
+ERROR - 2024-10-04 21:35:39 --> 404 Page Not Found: Public/uploads
+ERROR - 2024-10-04 21:35:39 --> 404 Page Not Found: Public/uploads
+ERROR - 2024-10-04 21:35:39 --> 404 Page Not Found: Public/uploads
+ERROR - 2024-10-04 21:35:59 --> 404 Page Not Found: Public/uploads
+ERROR - 2024-10-04 21:35:59 --> 404 Page Not Found: Public/uploads
+ERROR - 2024-10-04 21:35:59 --> 404 Page Not Found: Public/uploads
+ERROR - 2024-10-04 21:35:59 --> 404 Page Not Found: Public/uploads
+ERROR - 2024-10-04 21:35:59 --> 404 Page Not Found: Public/uploads
+ERROR - 2024-10-04 21:36:12 --> 404 Page Not Found: Public/uploads
+ERROR - 2024-10-04 21:36:12 --> 404 Page Not Found: Public/uploads
+ERROR - 2024-10-04 21:36:12 --> 404 Page Not Found: Public/uploads
+ERROR - 2024-10-04 21:36:12 --> 404 Page Not Found: Public/uploads
+ERROR - 2024-10-04 21:36:12 --> 404 Page Not Found: Public/uploads
+ERROR - 2024-10-04 21:36:17 --> 404 Page Not Found: Public/uploads
+ERROR - 2024-10-04 21:36:17 --> 404 Page Not Found: Public/uploads
+ERROR - 2024-10-04 21:36:17 --> 404 Page Not Found: Public/uploads
+ERROR - 2024-10-04 21:36:17 --> 404 Page Not Found: Public/uploads
+ERROR - 2024-10-04 21:36:17 --> 404 Page Not Found: Public/uploads
+ERROR - 2024-10-04 21:40:21 --> 404 Page Not Found: Public/uploads
+ERROR - 2024-10-04 21:40:57 --> 404 Page Not Found: Public/uploads
+ERROR - 2024-10-04 21:41:07 --> 404 Page Not Found: Public/uploads
+ERROR - 2024-10-04 21:42:24 --> 404 Page Not Found: Public/uploads
---
+++ application/logs/log-2024-11-28.php
@@ -0,0 +1,13 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2024-11-28 07:43:39 --> 404 Page Not Found: Public/uploads
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2024-11-28 07:43:39 --> 404 Page Not Found: Public/uploads
+ERROR - 2024-11-28 07:43:40 --> 404 Page Not Found: Public/uploads
+ERROR - 2024-11-28 07:43:40 --> 404 Page Not Found: Public/uploads
+ERROR - 2024-11-28 07:43:40 --> 404 Page Not Found: Public/uploads
+ERROR - 2024-11-28 07:44:10 --> 404 Page Not Found: Public/uploads
+ERROR - 2024-11-28 07:44:10 --> 404 Page Not Found: Public/uploads
+ERROR - 2024-11-28 07:44:11 --> 404 Page Not Found: Public/uploads
+ERROR - 2024-11-28 07:44:11 --> 404 Page Not Found: Public/uploads
